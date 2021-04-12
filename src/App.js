1c7159e59ccf3dfc784d1e837bd9ec8b718f9ee3
SkyZhangT <zhangtianyangsky@gmail.com>
2021-04-12 16:19:15 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-import ResponsiveDrawer from './components/drawer'
-import './App.css';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import ResponsiveDrawer from "./components/drawer";
+import "./App.css";
 
 const theme = createMuiTheme({
-  
-})
+	palette: {
+		primary: {
+			main: "#2e1667",
+		},
+	},
+	typography: {
+		fontFamily: ["Roboto"],
+		h4: {
+			fontWeight: 600,
+			fontSize: 28,
+			lineHeight: "2rem",
+		},
+		h6: {
+			fontWeight: 100,
+			lineHeight: "2rem",
+		},
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <ThemeProvider theme=theme>
-      </ThemeProvider>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ThemeProvider theme={theme}>
+				<ResponsiveDrawer />
+			</ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
